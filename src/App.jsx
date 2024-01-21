ce5f5ad03ee6506616ef627752f3261f237e2493
Palathip <windeesel365@gmail.com>
2024-01-21 19:26:43 +0700

    feat(JSX assignment): task

--- src/App.jsx
+++ src/App.jsx
@@ -12,26 +12,47 @@ function getCurrentDateTime() {
 }
 
 function IntroSection() {
+
+  const articleStyles = {
+    container: "article",
+    title: "article-title",
+    body: "article-body",
+    link: "article-link"
+  }
+
+  
+  /*ให้กำหนด className ให้กับ HTML Element*/
+
   return (
-    <div>
-      <h3>TechUp Thailand</h3>
-      <p>เตรียมพบกับหลักสูตรปั้นคุณเป็น Software Developer ภายใน 4 เดือน</p>
-      <a href="https://www.techupth.com/" target="blank">
+    <div className={articleStyles.container}>
+      <h3 className={articleStyles.title}>TechUp Thailand</h3>
+      <p className={articleStyles.body}>เตรียมพบกับหลักสูตรปั้นคุณเป็น Software Developer ภายใน 4 เดือน</p>
+      <a className={articleStyles.link} href="https://www.techupth.com/" target="blank">
         techupth.com
       </a>
       {/* ให้แสดงวันเวลาด้วย Function `getCurrentDateTime` ที่กำหนดไว้ให้ */}
+      <div className="bootcamp-start-time">วันเวลาเริ่มหลักสูตร คือ {getCurrentDateTime()}</div>
     </div>
   );
 }
 
+/*
+ใน JSX, ควรใช้ className แทน class เพื่อกำหนดคลาส CSS 
+เนื่องจาก class ใน JSX นั้นเป็นคำสงวนที่ใช้สำหรับการกำหนด class ของ JavaScript objects 
+ที่ใช้ในการกำหนดแอตทริบิวต์ของ Element นั้น ๆ ใน React และ JSX จะใช้ className แทน class
+
+นอกจากนี้ ควรใช้ htmlFor แทน for เมื่อกำหนด label ใน JSX เนื่องจาก for เป็นคำสงวนใน JavaScript 
+และ JSX จะใช้ htmlFor แทน จึงทำให้ได้โค้ดที่ถูกต้องตามหลักการเขียน JSX และ React
+*/
+
 function LoginForm() {
   return (
-    <div class="login-container">
-      <form class="login-form">
+    <div className="login-container">
+      <form className="login-form">
         <h2>Login</h2>
-        <label for="username">Username</label>
+        <label htmlFor="username">Username</label>
         <input type="text" id="username" name="username" />
-        <label for="password">Password</label>
+        <label htmlFor="password">Password</label>
         <input type="password" id="password" name="password" />
         <button type="submit">Login</button>
       </form>
